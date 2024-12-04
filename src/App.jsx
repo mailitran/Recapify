a9ef43960025dd64fad0dcc4f63dac3089f41bb1
Maili Tran <mailit@uw.edu>
2024-12-04 12:11:45 -0800

    Modified token refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,79 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { clientId, tokenEndpoint, saveTokens, logOutClick } from './AuthUtil.jsx';
 import Login from './Login.jsx';
 import Callback from './Callback.jsx';
 import Dashboard from './Dashboard.jsx';
 import Explore from './ExplorePage/Explore.jsx';
+import ErrorModal from './ErrorModal.jsx';
 
 function App() {
+  const [showErrorModal, setShowErrorModal] = useState(false);
+  const [error, setError] = useState(null);
+
+  // Automatically refresh the token before it expires
+  // The interval runs across all pages to ensure that the access token is 
+  // periodically checked and refreshed before expiration, maintaining the user's 
+  // authentication status throughout the app
+  useEffect(() => {
+    const intervalId = setInterval(async () => {
+      let expiry_date = new Date(localStorage.getItem('expiry_date'));
+      // Only checks expiry date if it exists (user has been authenticated)
+      if (expiry_date) {
+        const now = new Date();
+
+        const deadband = 5 * 60 * 1000; // Set a 5 minute deadband (5 minutes in milliseconds)
+        expiry_date = new Date(expiry_date.getTime() - deadband);
+
+        if (expiry_date <= now) {
+          // Token has expired, refresh it
+          const data = await refreshToken();
+          saveTokens(data);
+        }
+      }
+    }, 60000); // 1 minute in milliseconds
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  // Refresh the access token if expired
+  const refreshToken = async () => {
+    try {
+      const refresh_token = localStorage.getItem('refresh_token');
+
+      const payload = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams({
+          grant_type: 'refresh_token',
+          refresh_token: refresh_token,
+          client_id: clientId
+        }),
+      };
+
+      const response = await fetch(tokenEndpoint, payload);
+      const data = await response.json();
+
+      if (!data.access_token) {
+        throw new Error('Failed to refresh access token');
+      }
+
+      return data;
+    } catch (err) {
+      console.error(err);
+      setError("Unable to refresh token. Please try logging in again");
+      setShowErrorModal(true); // Show error modal if an error occurs
+    }
+  };
+
+  // Close the error modal and log out
+  const handleCloseErrorModal = () => {
+    setShowErrorModal(false);
+    logOutClick();
+  };
+
   return (
     <BrowserRouter>
       <Routes>
@@ -13,8 +82,14 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/explore" element={<Explore />} />
       </Routes>
+
+      <ErrorModal
+        show={showErrorModal}
+        error={error}
+        onHide={handleCloseErrorModal}
+      />
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
-import { clientId, tokenEndpoint, saveTokens, logOutClick } from './AuthUtil.jsx';
 import { Container, Row, Col } from 'react-bootstrap';
+import { logOutClick } from './AuthUtil.jsx';
 import NavigationBar from './NavigationBar.jsx';
 import ErrorModal from './ErrorModal.jsx';
 import TopMusic from './TopMusic.jsx';
@@ -15,25 +15,6 @@ function Dashboard() {
     // Fetch user data when the access token is available or refreshed
     useEffect(() => {
         getUserData();
-    }, []);
-
-    // Automatically refresh the token before it expires
-    useEffect(() => {
-        const intervalId = setInterval(async () => {
-            let expiry_date = new Date(localStorage.getItem('expiry_date'));
-            const now = new Date();
-
-            const deadband = 5 * 60 * 1000;  // Set a 5 minute deadband (5 minutes in milliseconds)
-            expiry_date = new Date(expiry_date.getTime() - deadband);
-
-            if (expiry_date <= now) {
-                // Token has expired, refresh it
-                const data = await refreshToken();
-                saveTokens(data);
-            }
-        }, 60000); // 1 minute in milliseconds
-
-        return () => clearInterval(intervalId);
     }, []);
 
     // Fetch user data
@@ -60,39 +41,7 @@ function Dashboard() {
         }
     }
 
-    // Refresh the access token if expired
-    const refreshToken = async () => {
-        try {
-            const refresh_token = localStorage.getItem('refresh_token');
-
-            const payload = {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                body: new URLSearchParams({
-                    grant_type: 'refresh_token',
-                    refresh_token: refresh_token,
-                    client_id: clientId
-                }),
-            }
-
-            const response = await fetch(tokenEndpoint, payload);
-            const data = await response.json();
-
-            if (!data.access_token) {
-                throw new Error('Failed to refresh access token');
-            }
-
-            return data;
-        } catch (err) {
-            console.error(err);
-            setError("Unable to refresh token. Please try logging in again");
-            setLoading(false);
-        }
-    }
-
-    // Close the error modal
+    // Close the error modal and log out
     const handleCloseErrorModal = () => {
         setShowErrorModal(false);
         logOutClick();
